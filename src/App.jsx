38374ee96c5de781d2c6c92756392889b55e272a
AguirreEze <aguirre.eze95@gmail.com>
2022-01-11 11:48:07 -0300

    feat conditional rendering based on loged state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+import { useApolloClient } from '@apollo/client'
 import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
+import Login from './components/Login'
 import NewBook from './components/NewBook'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [isLogged, setIsLogged] = useState(() => !!localStorage.getItem('library-user-token'))
+  const client = useApolloClient()
+
+  const logout = () => {
+    setIsLogged(false)
+    localStorage.removeItem('library-user-token')
+    client.resetStore()
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {isLogged && <button onClick={() => setPage('add')}>add book</button>}
+        {
+        isLogged
+          ? <button onClick={logout}>Logout</button>
+          : <button onClick={() => setPage('login')}>login</button>
+        }
       </div>
 
       <Authors
@@ -24,6 +39,12 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+      />
+
+      <Login
+        show={page === 'login'}
+        setIsLogged={setIsLogged}
+        setPage={setPage}
       />
 
     </div>
