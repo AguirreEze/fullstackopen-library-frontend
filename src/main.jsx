d5f175af26dcd0ac073909ff711fa86ec29ffb66
AguirreEze <aguirre.eze95@gmail.com>
2022-01-11 11:48:36 -0300

    feat authorization header logic

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
 
+const getAuth = () => {
+  const token = localStorage.getItem('library-user-token')
+  return token ? `bearer ${token}` : null
+}
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
+    headers: {
+      authorization: getAuth()
+    },
     uri: 'http://localhost:4000/'
   })
+
 })
 
 ReactDOM.render(
